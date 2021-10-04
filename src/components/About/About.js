07ccbd703f634054353f3243b7122b1c50fcbb4c
sifat-fahimul <mdsifatfeni@gmail.com>
2021-10-04 17:30:13 +0600

    final touch done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>this is about</h1>
+        <div className='bg-light'>
+            <div className='container'>
+                <div className="row mt-5 mb-5 pb-5 pt-5">
+                    <div className="col-6">
+                        <img src="http://ed.aislinthemes.com/app/uploads/sites/5/2017/10/091-510x365.jpg" alt="" />
+                    </div>
+                    <div className="col-6">
+                        <h1>WHY KINDERGARTEN IS
+                            THE BEST START?</h1>
+                        <h4>If your kindergarten is looking to acquire a new
+                            computer please refer</h4>
+                        <p>The first step in the real world of forex is opening
+                            an account with a broker. But be warned! Brokers
+                            love to hunt for noobs. Unfortunately, there are
+                            those out there. </p>
+                        <button className='btn btn-outline-success ps-5 pe-5 text-black fw-bold mt-4'>More</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,45 @@
+import { faCommentsDollar, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
+    const callIcon = <FontAwesomeIcon icon={faPhoneAlt}></FontAwesomeIcon>
+    const textIcon = <FontAwesomeIcon icon={faCommentsDollar}></FontAwesomeIcon>
     return (
         <div>
-            <h1>this is contact</h1>
+            <div className='contact-bg'>
+                <div className="container mt-5 text-white pt-5">
+                    <div className="mt-5 pt-5">
+                        <h1>Get in Touch</h1>
+                        <p>Want to get in touch? We'd love to have from you. Here's haw you can reach us.</p>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div className="container ">
+                <div className=" row  m-2">
+                    <div className="col-6 custom-container">
+                        <div className="custom-cart">
+                            <span className='fs-2'>{callIcon}</span>
+                            <h3>Talk to Us</h3>
+                            <p>Talk To Us is our annual awareness-raising campaign. Every July, our branches hold events in their communities to talk about their services.</p>
+                            <p className='mt-4 text-success f'>+00 1865 15454</p>
+
+                        </div>
+                    </div>
+                    <div className="col-6 custom-container">
+                        <div className="custom-cart">
+                            <span className='fs-2'>{textIcon}</span>
+                            <h3>Contact Student Query</h3>
+                            <p>The Student Enquiries Centre are piloting a live chat service from Tuesday 17th August to support students who have a question about their enrolment or pre-enrolment and need a quick answer. </p>
+                            <button className='btn btn-outline-danger mt-2'>Contact support</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
